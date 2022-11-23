465e401c50a04416ebaf23e56eea26b2b23cfb2d
Pubudu Janith <pubudujanith123@gmail.com>
2022-11-23 11:02:53 +0530

    Implement initial logic in js file

--- js/index.js
+++ js/index.js
@@ -1,2 +1,23 @@
-const tblStudents = document.getElementById("tbl-students");
 const btnNew = document.getElementById("btn-new");
+const btnSave = document.getElementById("btn-save");
+const btnClear = document.getElementById("btn-clear");
+const tblStudents = document.getElementById("tbl-students");
+
+const txtId = document.getElementById("txt-id");
+const txtName = document.getElementById("txt-name");
+const txtContact = document.getElementById("txt-contact");
+const txtAddress = document.getElementById("txt-address");
+const formStudent = document.getElementById("form-student");
+
+btnNew.addEventListener("click", () => {
+    [txtId, txtName, txtAddress, txtContact, btnSave, btnClear].forEach(element => {
+        element.disabled = false;
+    });
+    txtName.focus();
+    btnClear.click();
+    btnSave.innerText = "SAVE";
+});
+
+function generateNewStudentId() {
+    
+}
